88d77fe4775e9a5c9ee88278f63dc473e9e0e742
VikashChoudhary04 <62841254+VikashChoudhary04@users.noreply.github.com>
2023-10-30 14:05:02 +0000

    commit

--- basics/06_nums_and_math.js
+++ basics/06_nums_and_math.js
@@ -11,3 +11,24 @@ console.log(balance.toFixed(2)); // Upto 2 decimal places
 const otherNumber = 23.8966
 console.log(otherNumber.toPrecision(3));
 
+const hundreds = 1000000
+console.log(hundreds.toLocaleString()); //1,000,000,000
+console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++ Maths +++++++++
+console.log(Math); // Object
+console.log(Math.abs(-4)); // -4 = 4
+console.log(Math.round(4.3)); // 4
+console.log(Math.ceil(4.2)); // 4
+console.log(Math.floor(4.9)); // 4
+console.log(Math.min(4,3,2,1)); // 1
+console.log(Math.max(4,5,6,7)); // 7
+console.log(Math.random()); // givesvalue between 0 and 1
+console.log((Math.random()*10) + 1); // gives value between 1 and 10
+
+const min = 10
+const max = 20
+
+console.log(Math.random() * (max-min+1) + min); // get value between 10 and 20
+
+
