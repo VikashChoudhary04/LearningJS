a1a59e413eb1ace29e894dcf0ccc6020b96b97ba
VikashChoudhary04 <62841254+VikashChoudhary04@users.noreply.github.com>
2023-11-06 09:25:30 +0000

    commit

---
+++ 05_iterations/seven.js
@@ -0,0 +1,11 @@
+//  Maps
+const myNumbers = [1,2,3,4,5,6,7,8,9,10]
+
+const newNums = myNumbers.map( (num) => num + 10) // { return num + 10 } whenever open a scope just type return because it is mandatory
+console.log(newNums);
+
+// chaining
+
+const newNums = myNumbers.map((num) => num * 10).map( (num) => num + 1).filter( (num) => num >= 40)
+console.log(newNums);
+
